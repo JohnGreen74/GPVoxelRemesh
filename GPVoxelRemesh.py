4ce04dd4ee56d978277fda038479dac006a5fc30
JohnGreen74 <43453420+JohnGreen74@users.noreply.github.com>
2019-06-10 17:21:00 +0200

    Update GPVoxelRemesh.py

--- GPVoxelRemesh.py
+++ GPVoxelRemesh.py
@@ -3,8 +3,8 @@ bl_info = {
     "author": "Asch",
     "version": (1, 0),
     "blender": (2, 80, 0),
-    "location": "View3D > Add > Mesh > New Object",
-    "description": "Adds a new Mesh Object",
+    "location": "View3D > Object > GP_CNV_action",
+    "description": "GreasePensil to VoxelMesh",
     "warning": "",
     "wiki_url": "",
     "category": "A_GPVoxelRemesh", #category in add-ons, preferences
@@ -19,12 +19,15 @@ from mathutils import Vector
 
 #real actions
 def gpconvertor(self, context):
+    
     bpy.ops.object.mode_set(mode='OBJECT')  
     bpy.ops.gpencil.convert(type='CURVE', use_timing_data=True)
     bpy.ops.object.select_all(action='DESELECT')
     bpy.data.objects['GP_Layer'].select_set(True)
     OB = bpy.data.objects['GP_Layer']
     bpy.context.view_layer.objects.active = OB
+    
+    
     
     if self.merge == True:
         OB_Old = OB
@@ -59,23 +62,27 @@ def gpconvertor(self, context):
         bpy.ops.object.remesh()
     
     if self.merge == True:
-        OB = bpy.data.objects['VoxelMesh']
-        bpy.data.objects['VoxelMesh'].select_set(True)
-        bpy.ops.object.join()
-        bpy.ops.object.remesh()
-        #del OB_Old
-    
-    OB = bpy.data.objects['NewVoxelMesh']
-    bpy.data.objects['NewVoxelMesh'].select_set(True)
-    OB.name = "VoxelMesh"
+        OB = None
+        OB = bpy.data.objects.get("VoxelMesh")
+        if OB is None:
+            OB = bpy.data.objects['NewVoxelMesh']
+            bpy.data.objects['NewVoxelMesh'].select_set(True)
+            OB.name = "VoxelMesh" 
+            bpy.ops.object.remesh()
+        else:
+            bpy.data.objects['VoxelMesh'].select_set(True)
+            bpy.ops.object.join()
+            bpy.ops.object.remesh()
+            OB = bpy.data.objects['NewVoxelMesh']
+            bpy.data.objects['NewVoxelMesh'].select_set(True)
+            OB.name = "VoxelMesh"
     
     
 
 def gpfastcreate(self, context):
-    bpy.ops.object.mode_set(mode='OBJECT')  
     bpy.ops.object.gpencil_add(location=(0, 0, 0), type='EMPTY')
     bpy.ops.gpencil.paintmode_toggle()
-
+    
 
 
 
@@ -95,49 +102,29 @@ class OBJECT_OT_Asch_gp_to_mesh(Operator):
 
     def execute(self, context):
 
-        gpconvertor(self, context)
+        OB = None
+        OB = bpy.data.objects.get("GPencil")
+        if OB is None:  
+            gpfastcreate(self, context)
+        else:
+            gpconvertor(self, context)
 
         return {'FINISHED'}
 
-
-class OBJECT_OT_Asch_new_gp(Operator):
-    """Create a new GP Object"""
-    bl_idname = "mesh.gpfastcreate"  #id operator in keymap preferences
-    bl_label = "GP fast create" #name in keymap preferences
-    bl_options = {'REGISTER', 'UNDO'}
-    
-    #limit = bpy.props.FloatProperty(name="limit", default=0.1)
-    #bevel_depth = bpy.props.FloatProperty(name="bevel_depth*100", default=1)
-    #voxel_size = bpy.props.FloatProperty(name="voxel_size*100", default=20, min=1, max=100)
-    #smooth = bpy.props.BoolProperty(name="smooth", default=False)
-    
-
-    def execute(self, context):
-
-        gpfastcreate(self, context)
-
-        return {'FINISHED'}
 
 # Registration
-
 def add_object_button(self, context):
     self.layout.operator(
         OBJECT_OT_Asch_gp_to_mesh.bl_idname,
-        text="GP_CNV_action1",
+        text="GP_CNV_action",
         icon='PLUGIN')
 
-def add_object_button2(self, context):
-    self.layout.operator(
-        OBJECT_OT_Asch_new_gp.bl_idname,
-        text="GP_CNV_action2",
-        icon='PLUGIN')
 
 
 def register():
     bpy.utils.register_class(OBJECT_OT_Asch_gp_to_mesh)
     bpy.types.VIEW3D_MT_object.append(add_object_button)
-    bpy.utils.register_class(OBJECT_OT_Asch_new_gp)
-    bpy.types.VIEW3D_MT_object.append(add_object_button2)
+    
 
 
 def unregister():
